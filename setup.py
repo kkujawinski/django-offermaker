4f11ebd8050187cbbaaf7f22f0fa75da080c0cb3
Kamil Kujawinski <kamil@kujawinski.net>
2015-01-30 12:31:09 +0100

    version 0.9.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-offermaker',
-    version='0.9.4',
+    version='0.9.5',
     packages=['offermaker'],
     include_package_data=True,
     license='LGPL',
